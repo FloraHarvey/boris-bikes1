b22abbf0da478cb1206be36ea0865bb02fd34cc1
Katie Koschland <katiekoschland@hotmail.com>
2017-02-15 16:47:59 +0000

    Docking Station only releases bikes when available

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -3,10 +3,27 @@ require 'docking_station'
 describe DockingStation do
   it { is_expected.to respond_to :release_bike}
 
-  it 'releases working bikes' do
-      bike = subject.release_bike
-      expect(bike).to be_working
+describe '#release_bike' do
+  it 'releases a bike' do
+    bike = Bike.new
+    subject.dock_bike(bike)
+
+    expect(subject.release_bike).to eq bike
   end
+
+  it 'raises an error when there are no bikes available' do
+
+    expect{ subject.release_bike}.to raise_error 'No bikes available'
+  end
+ end
+
+
+
+  #
+  # it 'releases working bikes' do
+  #     bike = subject.release_bike
+  #     expect(bike).to be_working
+  # end
 
   it { is_expected.to respond_to(:dock_bike).with(1).argument }
 
