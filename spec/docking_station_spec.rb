b91d922494fbb2fdcb988400155d2b83a3f76ff7
Flora Harvey <fharvey555@gmail.com>
2017-02-15 12:30:48 +0000

    Made docking stations get bikes

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -8,15 +8,3 @@ it 'releases working bikes' do
     expect(bike).to be_working
 end
 end
-
-# describe DockingStation do
-#   describe '#release_bike' do
-#     it 'should create a new bike' do
-#       expect(DockingStation.release_bike).to eq (Bike)
-#     end
-#   end
-#   #it { subject.release_bike  be_a(Bike)}
-#   # it { is_expected.to respond_to :release_bike}
-# end
-#
-# #it { subject.call(1).should be_a(Person) }
